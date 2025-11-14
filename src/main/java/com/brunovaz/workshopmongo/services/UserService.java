4f353b9ec8b3268ea89d6a5f0c97c7cb564da770
Bruno Abreu <vazs.bruno@gmail.com>
2025-11-14 10:49:49 -0300

    feat: Inserir, deletar e atualizar User

--- src/main/java/com/brunovaz/workshopmongo/services/UserService.java
+++ src/main/java/com/brunovaz/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brunovaz.workshopmongo.domain.User;
+import com.brunovaz.workshopmongo.dto.UserDTO;
 import com.brunovaz.workshopmongo.repository.UserRepository;
 import com.brunovaz.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,30 @@ public class UserService {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public void delete(String id){
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
+    public User update (User obj){
+        User newObj= findById(obj.getId());
+        updateData(newObj, obj);
+        return userRepository.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
 
 }
